d47bf7ff0173b6a3ef9bfcca52cf4c7779393a6f
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-10-04 06:47:29 +0700

    add images route

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,5 +1,8 @@
 const spoonAPI = require('../api/spoonAPI')
 const spoonAPIKey = process.env.SPOON_API_KEY
+const googleAPI = require('../api/googleAPI')
+const googleAPIKey = process.env.GOOGLE_API_KEY
+const googleCSEId = process.env.GOOGLE_CSE_ID
 
 class RecipeController {
     static find(req, res, next) {
@@ -8,6 +11,19 @@ class RecipeController {
         .then(({data}) => {
             res.status(200).json(data)
         })
+        .catch(next)
+    }
+
+    static findImages(req, res, next) {
+        googleAPI.get(`?key=${googleAPIKey}&cx=${googleCSEId}&q=${req.body.recipe}`)
+        .then(({data}) => {
+            let images = []
+            console.log(data)
+            data.items.forEach(item => {
+                images.push(item.pagemap.cse_image[0].src)
+            })
+            res.status(200).json({images_src: images})
+        }) 
         .catch(next)
     }
 }
--- routers/recipeRouter.js
+++ routers/recipeRouter.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const RecipeController = require('../controllers/recipeController')
 
 router.post('/', RecipeController.find)
+router.post('/images', RecipeController.findImages)
 
 module.exports = router
