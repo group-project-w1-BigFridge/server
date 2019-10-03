f7f557e0557708392bf329d3943f42fa0d345175
dipadana <putudipadana@gmail.com>
2019-10-03 20:03:04 +0700

    Set Up

---
+++ app.js
@@ -0,0 +1,28 @@
+if(process.env.NODE_ENV === "development"){
+  require('dotenv').config()
+}
+
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+mongoose.connect(process.env.MONGOOSE_URL, {useNewUrlParser:true,useUnifiedTopology:true})
+.then(()=>{console.log('Database connected')})
+.catch(err => {console.log(err)})
+
+app.use(cors())
+app.use(morgan('tiny'))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.get('/', (req,res) => {
+  res.status(200).json({status:'connected'})
+})
+
+app.listen(port, () => {
+  console.log('App listen on port ' + port)
+})
