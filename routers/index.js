78d9df1d302bea764d7a987e531b2ecc6b55266b
dipadana <putudipadana@gmail.com>
2019-10-04 14:20:29 +0700

    Fix Bug

--- routers/index.js
+++ routers/index.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const UserRouter = require('../routers/userRouter')
 const recipeRouter = require('./recipeRouter')
 
+router.get('/', (req,res) => {
+  res.status(200).json({message:"Connected"})
+})
 router.use('/users', UserRouter)
 router.use('/recipes', recipeRouter)
 
